6a852f1e25b95be8195b9a8161d54f59de26d96a
Leonidastee <42331603+Leonidastee@users.noreply.github.com>
2018-08-24 12:31:32 +0800

    Beat Tapper Life = 10 glitch patch

--- game.cpp
+++ game.cpp
@@ -99,10 +99,13 @@ void minigame1_init()
 	g_door.m_cLocation.X = 40;
 	g_door.m_cLocation.Y = 13;
 	g_door.m_bActive = false;
-	if (Lives + int_stages/10 <= 99)
-		Lives += int_stages/10;
-	else
-		Lives = 99;
+	if (!g_bMinigame)
+	{
+		if (Lives + int_stages / 10 <= 99)
+			Lives += int_stages / 10;
+		else
+			Lives = 99;
+	}
 }
 void minigame2_init()
 {
