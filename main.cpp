9007e3f5d3467204f9a5bb7432fa83444b2b2233
Tan Yong Wee <42331460+chichupasta@users.noreply.github.com>
2018-08-29 11:56:37 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@ void mainLoop( void );
 // You should not be modifying this unless you know what you are doing
 int main( void )
 {
+	convertToString();
     mainLoop();  // main loop
     shutdown();  // do clean up, if any. free memory.
     
